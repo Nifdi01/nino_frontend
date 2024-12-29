44550cf0b5a65a13d9b49d68a4bb9d3b621c9199
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2024-12-29 16:27:17 +0400

    set up Vite with React; add Tailwind CSS configuration; create main app structure with routing and sidebar component; remove unused files

--- src/services/auth.js
+++
@@ -1,8 +0,0 @@
-import API from "./api";
-
-export const login = (credidentials) => API.post('/users/login/', credidentials);
-export const register = (data) => API.post('users/register'/, data);
-export const fetchUsers = () => API.get('/users/all/');
-export const fetchNews = () => API.get('/news/');
-export const fetchSources = () => API.get('/sources/');
-export const fetchKeywords = () => API.get('/keywords/');
