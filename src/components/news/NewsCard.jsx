052fbe1fa8b8cb8ee3e7741e4b39d24578211630
Nifdi Guliyev <87927314+Nifdi01@users.noreply.github.com>
2025-01-30 17:30:57 +0400

    feat: update NewsCard component to format title and date; adjust styling for improved layout

--- src/components/news/NewsCard.jsx
+++ src/components/news/NewsCard.jsx
@@ -6,7 +6,7 @@ const NewsCard = ({ product, style }) => {
   const date = new Date(product.published_at);
   const formattedDate = formatInTimeZone(date, "UTC", 'yyyy-MM-dd HH:mm');
   return (
-    <div style={style} className="px-2 pb-4"> {/* Padding for spacing */}
+    <div style={style} className="px-2">
       <a
         href={product.link}
         className="block p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 transition-colors duration-200"
@@ -18,9 +18,12 @@ const NewsCard = ({ product, style }) => {
         <p className='text-gray-400 text-sm'>{formattedDate}</p>
 
         {/* Title */}
-        <h5 className="mb-1 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {product.title}
+        <h5 className="mb-1 text-xl sm:text-lg md:text-xl lg:text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          {product.title.split(" ").slice(0, 8).join(" ")}
+          {product.title.split(" ").length > 8 ? " ..." : ""}
         </h5>
+
+
 
         {/* Source */}
         <div className="mb-2">
@@ -31,14 +34,14 @@ const NewsCard = ({ product, style }) => {
             {product.source.name}
           </span>
           {product.keywords && product.keywords.length > 0 ? (
-              product.keywords.map((keyword, index) => (
-                <span
-                  key={index}
-                  className="inline-block bg-green-100 text-green-800 text-xs px-2 py-1 ml-2 rounded-full"
-                >
-                  {keyword.name}
-                </span>
-              ))
+            product.keywords.map((keyword, index) => (
+              <span
+                key={index}
+                className="inline-block bg-green-100 text-green-800 text-xs px-2 py-1 ml-2 rounded-full"
+              >
+                {keyword.name}
+              </span>
+            ))
           ) : null}
         </div>
 
